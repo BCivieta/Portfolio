bd7427b4a968a0df7dedc9926149527b233e6692
Blanca Civieta Bermejo <blancivi@gmail.com>
2025-08-14 19:25:33 +0200

    legibilidad

--- mi-portfolio/src/components/Hero.tsx
+++ mi-portfolio/src/components/Hero.tsx
@@ -27,7 +27,6 @@ export default function Hero() {
         muted
         playsInline
         loop
-        poster="/video/poster.jpg"
       >
         <source src="/video/space.webm" type="video/webm" />
         <source src="/video/space.mp4" type="video/mp4" />
